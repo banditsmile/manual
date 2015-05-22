7284baa36903b09c961b063b90bcf9b6ac882964
banditsmile <banditsmile@qq.com>
2015-05-22 17:57:01 +0800

    离线处理

---
+++ .common/js/jb51.jquery.extend.js
@@ -0,0 +1,11 @@
+--2015-05-22 09:26:16--  http://shouce.jb51.net/jquery.extend.js
+Resolving shouce.jb51.net (shouce.jb51.net)... 115.231.236.106
+Connecting to shouce.jb51.net (shouce.jb51.net)|115.231.236.106|:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: unspecified [application/x-javascript]
+Saving to: `jquery.extend.js'
+
+     0K .                                                      3.40M=0s
+
+2015-05-22 09:26:17 (3.40 MB/s) - `jquery.extend.js' saved [1429]
+
---
+++ .common/js/jquery-1.3.2.js
@@ -0,0 +1,12 @@
+--2015-05-22 09:24:59--  http://libs.baidu.com/jquery/1.3.2/jquery.min.js
+Resolving libs.baidu.com (libs.baidu.com)... 183.61.111.49
+Connecting to libs.baidu.com (libs.baidu.com)|183.61.111.49|:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 57272 (56K) [application/x-javascript]
+Saving to: `jquery.min.js'
+
+     0K .......... .......... .......... .......... .......... 89% 2.21M 0s
+    50K .....                                                 100% 14.2M=0.02s
+
+2015-05-22 09:25:00 (2.43 MB/s) - `jquery.min.js' saved [57272/57272]
+
